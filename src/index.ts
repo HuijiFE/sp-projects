ae225fb19d4a90c786746346b1047b37d9a3f7c4
duduluu <duduluu@outlook.com>
2018-03-15 11:20:35 +0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,16 @@ import { now, today } from '@/common/util';
 const main: HTMLElement | null = document.querySelector('.parcel-demo .main');
 
 if (main) {
-  main.style.textAlign = 'center';
+  const section: HTMLElement = document.createElement('section');
+  const dateTime: HTMLElement = document.createElement('h2');
+  section.appendChild(dateTime);
+  main.appendChild(section);
+  setInterval(() => {
+    const timeNow: Date = new Date();
+    // tslint:disable-next-line:no-inner-html
+    dateTime.innerHTML = `${timeNow
+      .toLocaleDateString('zh-CN')
+      .split('/')
+      .join('-')} ${timeNow.toLocaleTimeString('zh-CN', { hour12: false })}`;
+  }, 1000);
 }
-
-// tslint:disable-next-line:no-console
-console.log(`Today is ${today()}, Now is ${now()}.`);
