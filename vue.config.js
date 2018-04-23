d9791acc78731b02316660ec5c1470fe90445385
duduluu <duduluu@outlook.com>
2018-04-23 15:07:03 +0800

    Update

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
-if (isProduction) {
-  console.log('Build for production');
-}
-
 module.exports = {
-  baseUrl: isProduction ? '//shouyou.178.com/css/up2018/' : '/',
+  baseUrl: '//shouyou.178.com/css/up2018/',
 };
